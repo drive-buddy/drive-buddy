cee9e44ce92d9019c94570803d56240e93af6103
adrenalin-42 <adrenalin582@gmail.com>
2022-10-12 00:26:22 +0300

    initial commit

---
+++ app/src/main/java/com/example/app2/ui/theme/Theme.kt
@@ -0,0 +1,45 @@
+package com.example.app2.ui.theme
+
+import android.graphics.Color
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = bgRed,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+
+    // Other default colors to override
+    background = bgRed,
+    surface = White,
+    onPrimary = White,
+    onSecondary = Black,
+    onBackground = Black,
+    onSurface = Black
+)
+
+@Composable
+fun App2Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
