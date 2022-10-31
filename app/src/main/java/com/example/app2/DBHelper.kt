d5fbb92ea32cd898373ab90aff961534544611ec
JustLearningThings <denismacio@outlook.com>
2022-10-31 09:45:46 +0200

    add getUsers method to DBHelper

--- app/src/main/java/com/example/app2/DBHelper.kt
+++ app/src/main/java/com/example/app2/DBHelper.kt
@@ -90,4 +90,70 @@ class DBHelper {
             .addOnSuccessListener { documents -> callback(documents) }
             .addOnFailureListener { e -> Log.w(TAG, "Error getting orders.", e) }
     }
+
+    fun getUsers(
+        callback: (data: QuerySnapshot) -> Unit,
+        type: String? = null, // driver or passenger
+        firstName: String? = null,
+        lastName: String? = null,
+        username: String? = null,
+        email: String? = null,
+        gender: Char? = null,
+        phoneNumber: String? = null,
+        yearsOfXp: Int? = null,
+        age: Int? = null,
+        rank: Int? = null,
+        carModel: String? = null,
+        carNumber: String? = null,
+        uid: String? = null,
+        limit: Int = 20
+    ) {
+        // create the query
+        val driversRef = db.collection("users")
+        var query = driversRef.limit(limit.toLong())
+
+        if (type != null && (type.lowercase() == "passenger" || type.lowercase() == "driver"))
+            query = query.whereEqualTo("type", type)
+
+        if (firstName != null && firstName.length > 0)
+            query = query.whereEqualTo("firstName", firstName)
+
+        if (lastName != null && lastName.length > 0)
+            query = query.whereEqualTo("lastName", lastName)
+
+        if (username != null && username.length > 0)
+            query = query.whereEqualTo("username", username)
+
+        if (phoneNumber != null && phoneNumber.length > 0)
+            query = query.whereEqualTo("phoneNumber", phoneNumber)
+
+        if (email != null && email.length > 0)
+            query = query.whereEqualTo("email", email)
+
+        if (carModel != null && carModel.length > 0)
+            query = query.whereEqualTo("carModel", carModel)
+
+        if (carNumber != null && carNumber.length > 0)
+            query = query.whereEqualTo("carNumber", carNumber)
+
+        if (uid != null && uid.length > 0)
+            query = query.whereEqualTo("uid", uid)
+
+        if (yearsOfXp != null)
+            query = query.whereEqualTo("yearsOfXp", yearsOfXp)
+
+        if (age != null)
+            query = query.whereEqualTo("age", age)
+
+        if (rank != null)
+            query = query.whereEqualTo("rank", rank)
+
+        if (gender != null && (gender == 'M' || gender == 'F'))
+            query = query.whereEqualTo("gender", gender)
+
+        // execute the query
+        query.get()
+            .addOnSuccessListener { documents -> callback(documents) }
+            .addOnFailureListener { e -> Log.w(TAG, "Error getting orders.", e) }
+    }
 }
